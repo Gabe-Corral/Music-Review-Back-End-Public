20bf0a659361dde7d0a9dcbd74a1ce6c38584ef2
Gabe-Corral <78gabe.corral@gmail.com>
2020-10-10 19:17:55 -0500

    user login/logout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,19 @@ class UserController < ApplicationController
 
   def index
     users = User.all
-    render json: users, only: [:id, :username, password:], include: [:Reviews]
+    render json: users, only: [:id, :username, :password], include: [:reviews]
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.valid?
+      user.save
+    end
+  end
+
+  private
+  def user_params
+    params.require(:User).permit(:username, :password)
   end
 
 end
