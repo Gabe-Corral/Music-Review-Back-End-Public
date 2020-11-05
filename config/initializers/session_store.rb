684a3cadcf6c921773baf76c0d34086720e72e68
Gabe-Corral <78gabe.corral@gmail.com>
2020-11-05 14:18:05 -0600

    last commit before deploy

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_Mod-5-Back-End", domain: "jdh-authentication-app-api.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_gabes-music-backend", domain: ""
 else
-  Rails.application.config.session_store :cookie_store, key: "_Mod-5-Back-End"
+  Rails.application.config.session_store :cookie_store, key: "_gabes-music-backend"
 end
